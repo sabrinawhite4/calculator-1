92cfbb24c4287f4728a727bd45f0acde17ce98e8
sabrinawhite4 <sabrinawhite4@me.com>
2021-05-24 16:06:09 -0600

    first

---
+++ calc.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);	
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);	
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);	
+	}
+
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));		
+	}
+
+	reader.close()
+});
+
+console.log()
